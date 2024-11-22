0648ea49faf08fb4e27421c1d9ee569932b1d38a
BarbaraCipolla6 <128473393+BarbaraCipolla6@users.noreply.github.com>
2024-11-22 18:37:43 -0300

    Update pokeAPI.py

--- pokeAPI.py
+++ pokeAPI.py
@@ -12,25 +12,32 @@ def obtener_pokemon(nombre):
         pokemon = response.json()
         nombre_pokemon = pokemon["name"].capitalize()
         id_pokemon = pokemon["id"]
+        habilidades=",".join([habilidad["ability"]["name"]for habilidad in pokemon["abilities"]])
         tipos = ",".join([tipo["type"]["name"] for tipo in pokemon["types"]])
         url_imagen = pokemon["sprites"]["front_default"]
         response_imagen = requests.get(url_imagen)
         img = Image.open(BytesIO(response_imagen.content))
-        mostrar_info_pokemon(nombre_pokemon, id_pokemon, tipos, img)
+        mostrar_info_pokemon(nombre_pokemon, id_pokemon, tipos, img,habilidades)
     else:
         messagebox.showerror("Error", "Pokemon no encontrado o nombre inválido")
     
-def mostrar_info_pokemon(nombre,id_pokemon,tipos,img):
+def mostrar_info_pokemon(nombre,id_pokemon,tipos,img,habilidades):
     for widget in ventana.winfo_children():
         widget.destroy()
     label_nombre = tk.Label(ventana, text=f"{nombre} (ID: {id_pokemon})",font=("Arial", 20))
     label_nombre.pack(pady=5)
+    
+    label_tipos =tk.Label(ventana,text=f"Tipos: {tipos}",font=("Arial",14))
+    label_tipos.pack(pady=5)
     
     img = img.resize((200,200))#redimensionar la imagen
     img_tk = ImageTk.PhotoImage(img)
     label_imagen = tk.Label(ventana, image=img_tk)
     label_imagen.image = img_tk
     label_imagen.pack(pady=10)
+    
+    label_habilidades = tk.Label(ventana,text=f"Habilidades: {habilidades}",font=("Arial",14))
+    label_habilidades.pack(pady=10)
     
 def buscar_pokemon():
     nombre = entry_nombre.get()
